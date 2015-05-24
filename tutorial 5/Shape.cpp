8afd5a4923c51f2ec457b89845b32cc1aa1941ff
RO169 <rowen.rungiah@gmail.com>
2015-05-25 01:57:27 +0200

    header file - namespace std not being permitted

--- tutorial 5/Shape.cpp
+++ tutorial 5/Shape.cpp
@@ -1,11 +1,40 @@
 #include "Shape.h"
+#include<string>
+//using namespace std;
+const int pi = 3.1416;
 
-
-Shape::Shape()
+Shape::Shape(int w = 0, int h = 0, string n = 'Shape')
 {
+
 }
 
-
-Shape::~Shape()
+string Circle::name()
 {
+
 }
+
+Circle::Circle(int w, int h, string n) : Shape(w, h){};
+float Circle::area()
+{
+
+}
+float Circle::perimeter()
+{
+
+}
+float Circle::draw(){}
+
+Rectangle(int w, int h, string n) : Shape(w, h){};
+string Rectangle::name(){}
+float Rectangle::area(){}
+float Rectangle::perimeter(){}
+float Rectangle::draw(){}
+
+Rectangle(int w, int h, string n) : Shape(w, h){};
+string Triangle::name(){}
+float Triangle::area(){}
+float Triangle::perimeter(){}
+float Triangle::draw(){}
+
+
+Shape::~Shape(){}
--- tutorial 5/Shape.h
+++ tutorial 5/Shape.h
@@ -9,16 +9,45 @@ class Shape
 protected:
 	int width;
 	int height;
+	string nameS;
 
 public:
-	Shape(int w, int h);
-	
+	Shape(int w, int h, string n);
 	virtual string name() = 0;
 	virtual float area() = 0;
 	virtual float perimeter() = 0;
 	virtual float draw() = 0;
- 
-	~Shape();
+ 	~Shape();
+};
+
+class Circle : public Shape
+{
+public:
+	Circle(int w, int h, string n) : Shape(w, h, n){};
+	string name();
+	float area();
+	float perimeter();
+	float draw();
+};
+
+class Rectangle : public Shape
+{
+public:
+	Rectangle(int w, int h, string n) : Shape(w, h, n){};
+	string name();
+	float area();
+	float perimeter();
+	float draw();
+};
+
+class Triangle : public Shape
+{
+public:
+	Triangle(int w, int h, string n) : Shape(w, h, n){};
+	string name();
+	float area();
+	float perimeter();
+	float draw();
 };
 
 #endif
