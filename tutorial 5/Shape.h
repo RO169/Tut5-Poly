c641c374b36a624822e6761b1745fb0ef37cc5d9
RO169 <rowen.rungiah@gmail.com>
2015-05-25 01:17:10 +0200

    created base class header file

--- tutorial 5/Shape.h
+++ tutorial 5/Shape.h
@@ -1,13 +1,23 @@
 #pragma once
 #ifndef Shape_H
 #define Shape_H
+#include<string>
+using namespace std;
 
 class Shape
 {
 protected:
+	int width;
+	int height;
 
 public:
-	Shape();
+	Shape(int w, int h);
+	
+	virtual string name() = 0;
+	virtual float area() = 0;
+	virtual float perimeter() = 0;
+	virtual float draw() = 0;
+ 
 	~Shape();
 };
 
