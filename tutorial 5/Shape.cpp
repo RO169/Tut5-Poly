9238f6149e1d9cac082548578c432645fac8ac52
RO169 <rowen.rungiah@gmail.com>
2015-05-25 02:14:08 +0200

    completed shape.cpp with exception of triangle perimeter function

--- tutorial 5/Shape.cpp
+++ tutorial 5/Shape.cpp
@@ -1,40 +1,60 @@
 #include "Shape.h"
 #include<string>
-//using namespace std;
+using namespace std;
 const int pi = 3.1416;
 
-Shape::Shape(int w = 0, int h = 0, string n = 'Shape')
+Shape::Shape(int w = 0, int h = 0, string n = "Shape")
 {
-
+	width = w;
+	height = h;
+	nameS = n;
 }
+Shape::~Shape(){}
 
+Circle::Circle(int w=0, int h=0, string n = "Circle") : Shape(w, h, n){};
 string Circle::name()
 {
-
+	return (nameS);
 }
-
-Circle::Circle(int w, int h, string n) : Shape(w, h){};
 float Circle::area()
 {
-
+	return(pi*width*width);
 }
 float Circle::perimeter()
 {
-
+	return (2 * pi*width);
 }
 float Circle::draw(){}
 
-Rectangle(int w, int h, string n) : Shape(w, h){};
-string Rectangle::name(){}
-float Rectangle::area(){}
-float Rectangle::perimeter(){}
+Rectangle::Rectangle(int w = 0, int h = 0, string n = "Rectangle") : Shape(w, h, n){};
+string Rectangle::name()
+{
+	return(nameS);
+}
+float Rectangle::area()
+{
+	return(height*width);
+}
+float Rectangle::perimeter()
+{
+	return (2 * height*width);
+}
 float Rectangle::draw(){}
 
-Rectangle(int w, int h, string n) : Shape(w, h){};
-string Triangle::name(){}
-float Triangle::area(){}
-float Triangle::perimeter(){}
+Triangle::Triangle(int w, int h, string n = "Triangle") : Shape(w, h, n){};
+string Triangle::name()
+{
+	return(nameS);
+}
+float Triangle::area()
+{
+	return(width*height / 2);
+}
+float Triangle::perimeter()
+{
+	return (2*height+width); //this formula is incorrect
+}
 float Triangle::draw(){}
 
 
-Shape::~Shape(){}
+
